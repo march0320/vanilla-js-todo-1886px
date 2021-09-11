f678c13716a8722503fa493cdaf55228d2169ff3
march0320 <march18529@gmail.com>
2021-09-11 15:16:31 +0900

    todolist-index

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+const Input= document.getElementById("Input");
+const addButton= document.getElementById("add-button");
+const todoList=document.getElementById("todoList");
 
+function addToDo() {
+    if(Input.value!=null){
+        const li=document.createElement("li");
+        const delBtn=document.createElement("button");
+        const span =document.createElement("span");
+        delBtn.innerText="x";
+        span.value=Input.value;
+        li.appendChild(span);
+        li.appendChild(delBtn);
+        todoList.appendChild(li);
+        Input.value="";
+
+    }
+}
+
+function delToDO(event) {
+    var delList = event.target.parentNode;
+    delList.remove();
+}
+
+function toggle(event){
+    if (event.target.style.textDecoration !=="line-through"){
+        event.target.style.textDecoration ="line-through";
+    }else{
+        event.target.style.textDecoration ="none";
+    }
+}
