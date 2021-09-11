502a16a9fd27a4bcd7ab4e6365f2d75367c5e20b
march0320 <march18529@gmail.com>
2021-09-11 19:07:00 +0900

    todolist-js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+const Input= document.getElementById("Input");
+const addButton= document.getElementById("add-button");
+const toDoList=document.getElementById("list");
 
+function addToDo() {
+    if(Input.value!=null){
+        const li=document.createElement("li");
+        const delBtn=document.createElement("button");
+        const span =document.createElement("span");
+        delBtn.className="del-btn";
+        delBtn.textContent="x";
+        span.textContent=Input.value;
+        li.appendChild(span);
+        li.appendChild(delBtn);
+        toDoList.appendChild(li);
+        delBtn.addEventListener("click", delToDo);
+        span.addEventListener("click", toggle);
+        Input.value=" ";
+    }
+}
+
+function delToDo(event) {
+    var remove = event.target.parentNode;
+    var parentNode=remove.parentNode;
+    parentNode.removeChild(remove);
+}
+
+function toggle(event){
+    if (event.target.style.textDecoration !=="line-through"){
+        event.target.style.textDecoration ="line-through";
+    }else{
+        event.target.style.textDecoration ="none";
+    }
+}
